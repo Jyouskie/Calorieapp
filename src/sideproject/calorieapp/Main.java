1d6cdea664e5a7f3c5258fb76989b07755a9a7be
Jyouskie <youskie1994@gmail.com>
2024-05-14 22:22:38 -0500

    need to do more research on the calculations, seems not to be accurate the formula that I am using.

--- src/sideproject/calorieapp/Main.java
+++ src/sideproject/calorieapp/Main.java
@@ -28,6 +28,26 @@ public class Main {
             // initializing male maintenance calories
             int maleCalories = (int) (655 + (6.23 * userWeight) + (12.7 * userHeight) - (6.8 * userAge));
 
+            System.out.println("1. Not active");
+            System.out.println("2. Light active (1-3 days of exercise)");
+            System.out.println("3. Moderately active (moderate exercise 3-5 days)");
+            System.out.println("4. Very active (heavy exercise 6-7 days)");
+            System.out.println("4. Extremely active (strenuous training 2 times a day)");
+            System.out.print("Please enter the number that represents how active are you? ");
+            int activity = scanner.nextInt();
+
+            if (activity == 1) {
+                maleCalories = (int) (maleCalories * 1.2);
+            } else if (activity == 2) {
+                maleCalories = (int) (maleCalories * 1.375);
+            } else if (activity == 3) {
+                maleCalories = (int) (1.55 * maleCalories);
+            } else if (activity == 4) {
+                maleCalories = (int) (maleCalories * 1.725);
+            }else {
+                maleCalories = (int) (maleCalories * 1.9);
+            }
+
             // printing out maintenance calories
             System.out.printf("Your maintenance calories is: %d %n", maleCalories);
         }
@@ -43,11 +63,31 @@ public class Main {
             System.out.print("Please enter your age: ");
             int userAge = scanner.nextInt();
 
-            // initializing male maintenance calories
+            // initializing female maintenance calories
             int femaleCalories = (int) (655 + (4.3 * userWeight) + (4.7 * userHeight) - (6.8 * userAge));
+            System.out.println("1. Not active");
+            System.out.println("2. Light activity (1-3 days of exercise)");
+            System.out.println("3. Moderately active (moderate exercise 3-5 days)");
+            System.out.println("4. Very active (heavy exercise 6-7 days)");
+            System.out.println("4. Extremely active (strenuous training 2 times a day)");
+            System.out.print("Please enter the number that represents how active are you? ");
+            int activity = scanner.nextInt();
+
+            if (activity == 1) {
+                femaleCalories = (int) (femaleCalories * 1.2);
+            } else if (activity == 2) {
+                femaleCalories = (int) (femaleCalories * 1.375);
+            } else if (activity == 3) {
+                femaleCalories = (int) (femaleCalories * 1.55);
+            } else if (activity == 4) {
+                femaleCalories = (int) (femaleCalories * 1.725);
+            }else {
+                femaleCalories = (int) (femaleCalories * 1.9);
+            }
+
 
             // printing out maintenance calories
-            System.out.printf("Your maintenance calories is: %d %n", femaleCalories);
+            System.out.printf("Your maintenance calories is with: %d %n", femaleCalories);
         }
 
 
